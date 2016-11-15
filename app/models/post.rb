61e7ce3e4a532a360924b46954064e1f6d32ae88
Okeke Arinze <trent9002@gmail.com>
2016-11-15 10:12:18 +0100

    Add comment

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ApplicationRecord
 	belongs_to :user
+	has_many :comments, dependent: :destroy
 	validates :user_id, presence: true
 	validates :avatar, presence: true
 	has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
