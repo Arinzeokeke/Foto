4755a1d8875adc35ef6a4cbfb3f6dc65e8b0bbde
Okeke Arinze <trent9002@gmail.com>
2016-11-15 02:17:45 +0100

    Finish basic crud

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,9 @@
 class PostsController < ApplicationController
+
+	before_action :set_post, only: [:show, :edit, :update, :destroy]
+
 	def index
+		@posts = Post.all
 	end
 
 	def new
@@ -9,14 +13,49 @@ class PostsController < ApplicationController
 	def create
 		@post = Post.create(post_params)
 		if @post.save
+			flash[:success] = "Your post has been created!"
 			redirect_to posts_path
+		else
+			flash.now[:alert] = "Your new post couldn't be created! Please check the form."
+			render("new")
 		end
 		
 	end
 
+	def show
+		
+	end
+
+	def edit
+
+	end
+
+	def update
+		
+		if @post.update_attributes(post_params)
+      		flash[:success] = "Your post has been updated!"
+      		redirect_to  post_path(@post.id)
+    	else
+    		flash.now[:alert] = "Your new post couldn't be updated! Please check the form."
+      		render('edit')
+      	end
+    end
+
+    def destroy
+    	@post.destroy
+    	flash[:success] = "Your post has been deleted"
+    	redirect_to  posts_path
+    	else
+
+    end
+
 	private
 
 	def post_params
-		params.require(:post).permit(:image, :caption)
+		params.require(:post).permit(:avatar, :caption)
+ 	end
+
+ 	def set_post
+ 		@post = Post.find(params[:id])
  	end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
-	validates :image, presence: true
-	has_attached_file :image, styles: { :medium => "640x" }
-	validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+	validates :avatar, presence: true
+	has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+    validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 end
