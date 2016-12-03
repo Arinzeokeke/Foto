496c7a9db47dc62c7f4ec5317476c9e22f6bd32f
Okeke Arinze <trent9002@gmail.com>
2016-12-04 00:48:50 +0100

    style login signup edit forms

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,3 +7,11 @@ module ApplicationHelper
 			}[flash_type.to_sym] || flash_type.to_s
 		end
 	end
+
+	def form_image_select(post)
+		if post.avatar.exists?
+			return image_tag post.avatar.url(:medium), id: 'image-preview', class: 'img-responsive'
+		else
+			image_tag 'placeholder.jpg', id: 'image-preview', class: 'img-responsive'
+		end
+	end
--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,7 @@ class Post < ApplicationRecord
 	has_many :comments, dependent: :destroy
 	validates :user_id, presence: true
 	validates :avatar, presence: true
+	validates_length_of :caption, {:maximum => 300, :minimum => 3}
 	has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
     validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 end
